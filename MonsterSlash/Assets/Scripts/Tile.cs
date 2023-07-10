5cf7dca0c4e89b36492be3f524b1059cc5cb47b0
nakrekarpay1245 <rknyprk2001@gmail.com>
2023-07-10 10:25:00 +0300

    Tile System Added
    Tile, TileManager, TileState

---
+++ MonsterSlash/Assets/Scripts/Tile.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    private TileState _tileState;
+    public TileState TileState
+    {
+        get { return _tileState; }
+        set { _tileState = value; }
+    }
+
+    private Vector2Int _tileGridPosition;
+    public Vector2Int TileGridPosition
+    {
+        get { return _tileGridPosition; }
+        set { _tileGridPosition = value; }
+    }
+
+    [SerializeField]
+    private Sprite _emptyTile;
+    [SerializeField]
+    private Sprite _selectedTile;
+
+    private SpriteRenderer _spriteRendererComponent;
+
+    private void Awake()
+    {
+        _spriteRendererComponent = GetComponent<SpriteRenderer>();
+    }
+
+    public void Deactivate()
+    {
+        Destroy(gameObject);
+    }
+
+    //public void SetMonster(Monster newMonster)
+    //{
+    //    if (newMonster != null)
+    //    {
+    //        EmptyTile();
+    //        monster = newMonster;
+    //    }
+    //}
+
+    //public void FallMonster()
+    //{
+    //    monster.Fall();
+    //}
+
+    //public Monster GetMonster()
+    //{
+    //    return monster;
+    //}
+
+    //public MonsterType GetMonsterType()
+    //{
+    //    return monster.GetMonsterType();
+    //}
+
+    //public void TakeDamage()
+    //{
+    //    monster.TakeDamage();
+    //}
+
+    public void EmptyTile()
+    {
+        _spriteRendererComponent.sprite = _emptyTile;
+    }
+
+    public void SelectTile()
+    {
+        _spriteRendererComponent.sprite = _selectedTile;
+    }
+}
+
+public enum TileState
+{
+    Player,
+    Monster,
+    Obstacle,
+    Empty
+}
+
---
+++ MonsterSlash/Assets/Scripts/TileManager.cs
@@ -0,0 +1,86 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoSingleton<TileManager>
+{
+    [Header("Tile Manager Parameters")]
+    [Header("References")]
+    [Tooltip("The tile object that will create the grid")]
+    [SerializeField]
+    private Tile _tilePrefab;
+
+    [Header("Grid Parameters")]
+    [Header("Grid Scale")]
+    [Tooltip("Grid horizontal scale")]
+    [SerializeField]
+    private int _gridWidth;
+
+    [Tooltip("Grid vertical scale")]
+    [SerializeField]
+    private int _gridHeight;
+
+    [SerializeField]
+    private Vector2Int playerCharacterPosition;
+
+    private Tile[,] _tileGrid;
+    public Tile[,] TileGrid
+    {
+        get { return _tileGrid; }
+        private set { }
+    }
+
+    private List<Tile> _activeTileList;
+    public List<Tile> ActiveTileList
+    {
+        get { return _activeTileList; }
+        private set { }
+    }
+
+    private void Awake()
+    {
+        _tileGrid = new Tile[_gridWidth, _gridHeight];
+        _activeTileList = new List<Tile>();
+        GenerateTiles();
+    }
+
+    private void GenerateTiles()
+    {
+        int index = 0;
+        for (int x = 0; x < _gridWidth; x++)
+        {
+            for (int y = 0; y < _gridHeight; y++)
+            {
+                Vector2 gridOffset = new Vector2(_gridWidth / 2f, _gridHeight / 2f) * -1f +
+                    new Vector2(0.5f, 0.5f);
+
+                Vector2 tilePosition = (new Vector2(x, y) + gridOffset);
+
+                Tile generatedTile = Instantiate(_tilePrefab, tilePosition, Quaternion.identity,
+                    transform);
+
+                Vector2Int tileGridPosition = new Vector2Int(x, y);
+
+                generatedTile.TileGridPosition = tileGridPosition;
+
+                generatedTile.name = "Tile " + index;
+                //generatedTile.SetIndex(index);
+                index++;
+
+                _tileGrid[x, y] = generatedTile;
+                _activeTileList.Add(generatedTile);
+
+                if (playerCharacterPosition.x == x && playerCharacterPosition.y == y)
+                {
+                    //PlayerCharacter.singleton.transform.position = generatedTile.transform.position;
+                    generatedTile.TileState = TileState.Player;
+                }
+                else
+                {
+                    generatedTile.TileState = TileState.Empty;
+                }
+            }
+        }
+    }
+
+
+}
