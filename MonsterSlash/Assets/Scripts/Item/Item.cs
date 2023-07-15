028b6c969459380e01980f54379cb415539e87df
nakrekarpay1245 <rknyprk2001@gmail.com>
2023-07-15 12:47:29 +0300

    Move Function pulled in Item

--- MonsterSlash/Assets/Scripts/Item/Item.cs
+++ MonsterSlash/Assets/Scripts/Item/Item.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public abstract class Item : MonoBehaviour, IMoveable, IInteractable
@@ -5,9 +6,36 @@ public abstract class Item : MonoBehaviour, IMoveable, IInteractable
     [SerializeField]
     private ItemType _itemType;
     public ItemType ItemType { get => _itemType; private set { } }
-
-    public abstract void Move(Vector2 targetPosition);
     public abstract void Interact();
+
+    public virtual void Move(Vector2 targetPosition)
+    {
+        StopCoroutine(MoveRoutine(targetPosition));
+        StartCoroutine(MoveRoutine(targetPosition));
+    }
+
+    /// <summary>
+    /// This coroutine smoothly moves the object from its current position to the target position 
+    /// over a specified fall time.
+    /// </summary>
+    /// <param name="targetPosition"></param>
+    /// <returns></returns>
+    public IEnumerator MoveRoutine(Vector2 targetPosition)
+    {
+        Vector3 startPosition = transform.position;
+        float distance = Mathf.Abs(startPosition.y - targetPosition.y);
+
+        float elapsedTime = 0f;
+        float fallTime = GameSettings.singleton.TIME_1 * distance;
+
+        while (elapsedTime < fallTime)
+        {
+            float t = elapsedTime / fallTime;
+            transform.position = Vector3.Lerp(startPosition, targetPosition, t);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
 }
 
 public enum ItemType
--- MonsterSlash/Assets/Scripts/Item/Weapon.cs
+++ MonsterSlash/Assets/Scripts/Item/Weapon.cs
@@ -43,30 +43,6 @@ public class Weapon : Item, IActivetable
 
     public override void Move(Vector2 targetPosition)
     {
-        StopCoroutine(MoveRoutine(targetPosition));
-        StartCoroutine(MoveRoutine(targetPosition));
-    }
-
-    /// <summary>
-    /// This coroutine smoothly moves the object from its current position to the target position 
-    /// over a specified fall time.
-    /// </summary>
-    /// <param name="targetPosition"></param>
-    /// <returns></returns>
-    public IEnumerator MoveRoutine(Vector2 targetPosition)
-    {
-        Vector3 startPosition = transform.position;
-        float distance = Mathf.Abs(startPosition.y - targetPosition.y);
-
-        float elapsedTime = 0f;
-        float fallTime = GameSettings.singleton.TIME_1 * distance;
-
-        while (elapsedTime < fallTime)
-        {
-            float t = elapsedTime / fallTime;
-            transform.position = Vector3.Lerp(startPosition, targetPosition, t);
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
+        base.Move(targetPosition);
     }
 }
